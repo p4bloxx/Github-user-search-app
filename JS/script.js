6525638c0fc6078791aacb00add59ca27c437a76
Pablo Dettori <106544349+p4bloxx@users.noreply.github.com>
2024-01-29 11:22:17 +0100

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,168 @@
+const toggleTheme = document.querySelector('#light-dark');
+const input = document.querySelector('input');
+const inputSearch = document.querySelector('#search');
+const searchBar = document.querySelector('.searchbar');
+const cardResult = document.querySelector('.card-result');
+const errorMsg = document.querySelector('#error-msg');
+const pic = document.querySelector('.profile-pic-box img');
+const picDefaultURL = 'ASSETS/Images/user.png';
+const nameUser = document.querySelector('#name');
+const username = document.querySelector('#username a');
+const iscription = document.querySelector('#iscription');
+const bio = document.querySelector('#bio');
+const repos = document.querySelector('#repos');
+const followers = document.querySelector('#followers');
+const following = document.querySelector('#following');
+const local = document.querySelector('#location');
+const website = document.querySelector('#website a');
+const twitterProfile = document.querySelector('#twitter-profile');
+const company = document.querySelector('#company');
+
+//to switch light/dark mode and detected user's preference
+function toggleDarkMode() {
+  if (document.documentElement.classList.contains("light")) {
+    document.documentElement.classList.remove("light")
+    document.documentElement.classList.add("dark")
+    cardResult.classList.remove('box-shadow-active')
+    searchBar.classList.remove('box-shadow-active')
+    toggleTheme.firstElementChild.style.display = 'inline'
+    toggleTheme.lastElementChild.style.display = 'none'
+  } else if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark")
+    document.documentElement.classList.add("light")
+    cardResult.classList.add('box-shadow-active')
+    searchBar.classList.add('box-shadow-active')
+    toggleTheme.firstElementChild.style.display = 'none'
+    toggleTheme.lastElementChild.style.display = 'inline'
+  }
+}
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.documentElement.classList.add("dark")
+  cardResult.classList.remove('box-shadow-active')
+  searchBar.classList.remove('box-shadow-active')
+  toggleTheme.firstElementChild.style.display = 'inline'
+  toggleTheme.lastElementChild.style.display = 'none'
+} else {
+  document.documentElement.classList.add("light")
+  cardResult.classList.add('box-shadow-active')
+  searchBar.classList.add('box-shadow-active')
+  toggleTheme.firstElementChild.style.display = 'none'
+  toggleTheme.lastElementChild.style.display = 'inline'
+}
+
+toggleTheme.addEventListener('click', toggleDarkMode)
+
+
+function startSearch(currentInput){
+  const newData = async () => {
+    try{
+      const apiAddress = await fetch(`https://api.github.com/users/${currentInput}`)
+  
+      if(!apiAddress.ok){
+        if(apiAddress.status === 404){
+          errorMsg.style.opacity = 1
+          throw new Error ('Data not found');
+        } else if(apiAddress.status === 500){
+          throw new Error ('problem of server');
+        } else {
+          throw new Error ('Network response was not ok')
+        }
+      } else {
+        errorMsg.style.opacity = 0
+      }
+      
+      const response = await apiAddress.json()
+      const currentName = response.name;
+      const currentUserName = response.login;
+      const createdAt = response.created_at;
+      let convertDate = new Date(`${createdAt}`)
+      let currentMonth = convertDate.getMonth();
+      let currentDay = convertDate.getDate();
+      let currentYear = convertDate.getFullYear();
+      const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+      const toMonth = months[currentMonth]
+      const avatarUrl = response.avatar_url;
+      const currentBio = response.bio;
+      const currentRepos = response.public_repos;
+      const currentFollowers = response.followers;
+      const currentFollowing = response.following;
+      const currentLocation = response.location;
+      const currentTwitter = response.twitter_username;
+      const currentWebsite = response.blog;
+      const currentCompany = response.company;
+      const urlProfile = response.html_url;
+  
+      if(avatarUrl != ''){
+        pic.setAttribute('src', `${avatarUrl}`)
+      }
+  
+      nameUser.innerHTML = currentName;
+  
+      username.innerHTML = `@${currentUserName}`
+      username.setAttribute('href', `${urlProfile}`)
+      iscription.innerHTML = `Joined ${currentDay} ${toMonth} ${currentYear}`
+  
+      if(currentBio != null){
+        bio.innerHTML = currentBio;
+      } else {
+        bio.innerHTML = 'This profile has no bio';
+        bio.style.opacity = 0.75;
+      }
+      
+      if(currentRepos != null){
+        repos.innerHTML = currentRepos;
+      }
+      
+      if(currentFollowers != null){
+        followers.innerHTML = currentFollowers
+      }
+      
+      if(currentFollowing != null){
+        following.innerHTML = currentFollowing
+      }
+  
+      if(currentLocation == null){
+        local.innerHTML = 'Not Available'
+        local.classList.add('opacity')
+      } else{
+        local.innerHTML = currentLocation;
+      }
+  
+      if(currentTwitter == null){
+        twitterProfile.innerHTML = 'Not Available'
+        twitterProfile.classList.add('opacity')
+      }else {
+        twitterProfile.innerHTML = currentTwitter
+      }
+  
+      if(currentWebsite == ''){
+        website.innerHTML = 'Not Available'
+        website.classList.add('opacity')
+      } else {
+        website.innerHTML = currentWebsite
+        website.setAttribute('target', '_blank')
+        website.setAttribute('href', `https://www.${currentWebsite}`)
+      }
+  
+      if(currentCompany == null){
+        company.innerHTML = 'Not Available'
+        company.classList.add('opacity')
+      } else {
+        company.innerHTML = currentCompany
+      }
+  
+      console.log(response)
+    } catch(e){
+      console.error(e)
+    }
+    
+  }
+  newData()
+}
+
+inputSearch.addEventListener('click', () => {
+  let currentInput = input.value;
+  startSearch(currentInput)
+  input.value = ''
+})
---
+++ README.md
@@ -0,0 +1,72 @@
+# Frontend Mentor - GitHub user search app solution
+
+This is a solution to the [GitHub user search app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/github-user-search-app-Q09YOgaH6). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Search for GitHub users by their username
+- See relevant user information based on their search
+- Switch between light and dark themes
+- **Bonus**: Have the correct color scheme chosen for them based on their computer preferences. _Hint_: Research `prefers-color-scheme` in CSS.
+
+### Screenshot
+
+![](./SCREENSHOTS/mobile%20view.png)
+![](./SCREENSHOTS/tablet%20view.png)
+![](./SCREENSHOTS/desktop%20view.png)
+![](./SCREENSHOTS/desktop%20view-light%20mode.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- JS vanilla
+- Sass(preprocessor css)
+- Cube css
+
+### What I learned
+
+hello everyone ;) with this challenge I continued the practice regarding the use of **API**, I had some small problems managing some of the data shown on the screen, but nothing complicated. Overall I think everything went very well ;)
+
+### Continued development
+
+I will continue to create challenges of this type, regarding data management through APIs, and at the same time I will continue with other types of challenges that have a good level of training regarding the use of **JS**.
+
+### Useful resources
+
+## Author
+
+- Frontend Mentor - [@p4bloxx](https://www.frontendmentor.io/profile/p4bloxx)
+- Github - [p4abloxx](https://github.com/p4bloxx)
+
+## Acknowledgments
